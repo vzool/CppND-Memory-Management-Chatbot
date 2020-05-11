4415e5a0c229d5abf1e32b819f5e34a176da1a9d
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2020-05-11 07:00:36 +0300

    Task #1

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic = std::make_unique<ChatLogic>(); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -134,9 +134,11 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
 {
     //// STUDENT CODE
     ////
-
-    delete _chatLogic;
-
+    auto manual = _chatLogic.release();
+    
+    if(manual != nullptr){
+        delete manual;
+    }
     ////
     //// EOF STUDENT CODE
 }
